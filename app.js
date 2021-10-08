58a2f0fe5dead6bccb2987ff2042656b63f970e3
Kyle Rego <regoky@outlook.com>
2021-10-08 11:35:27 -0400

    fix overItself function

--- app.js
+++ app.js
@@ -31,6 +31,19 @@ function renderSnake(snake){
     snake.forEach(renderSnakeSegment);
 
 }
+// ->
+// displays the game over message in the HTML
+function displayGameOver(){
+    let gameOverMessage = document.createElement("div");
+    gameOverMessage.style.position = "relative";
+    gameOverMessage.style.top = "350px"
+    gameOverMessage.style.left = "250px"
+    gameOverMessage.textContent = "Game Over!";
+    gameOverMessage.style.fontSize = "24px";
+    gameOverMessage.style.color = "red";
+    snakeGame.appendChild(gameOverMessage);
+
+}
 
 // SnakeSegment -> SnakeSegment
 // moves the snake segment according to its direction
@@ -68,60 +81,74 @@ function moveSnake(snake){
 }
 
 // Snake -> Boolean
-// returns false if the snake hits itself or a wall
+// returns true if the snake hits itself or a wall
+// otherwise returns false
 function gameOver(snake){
     let firstSegment = snake[0];
     x = firstSegment.x;
     y = firstSegment.y;
-    let overBorder = (x >= 400) || (x <= 0);
-    return overBorder;
+    let overBorderX = (x >= 400) || (x <= -10);
+    let overBorderY = (y >= 400) || (y <= -10);
+    let snakeOverItself = overItself(snake);
+    return snakeOverItself || overBorderX || overBorderY ;
 }
 
-// Snake -> Snake
-// sets up a key event handler which changes the direction
-// of the first snake segment according to the arrow key pressed
-function addKeyHandler(snake){
-    document.addEventListener('keydown', function(key){
-        let keyValue = key.key;
-        let firstSegment = snake[0]
-        switch(keyValue){
-            case 'ArrowUp':
-                firstSegment.dir = "up";
-                break;
-            case 'ArrowDown':
-                firstSegment.dir = "down";
-                break;
-            case 'ArrowLeft':
-                firstSegment.dir = "left";
-                break;
-            case 'ArrowRight':
-                firstSegment.dir = "right";
-                break;
+// Snake -> Boolean
+// returns true if the snake's head is on top of the snake
+// otherwise returns false
+function overItself(snake){
+    let firstSegment = snake[0];
+    for (const snakeSegment of snake.slice(1)){
+        if (snakeSegmentEquals(snakeSegment, firstSegment)){
+            return true;
         }
-        snake[0] = firstSegment;
-    })
-    return snake;
+    }
+    return false;
 }
 
-
-console.log(moveSnake([ {x:40, y:40, dir:"right"}, {x:30, y:40, dir:"right"} ]))
-
-let mainSnake = [ {x:40, y:40, dir:"right"}, {x:30, y:40, dir:"right"} ]
+// SnakeSegment SnakeSegment -> Boolean
+// returns true if two snake segments occupy the same position
+function snakeSegmentEquals(snakeSegment1, snakeSegment2){
+    let xEquals = snakeSegment1.x === snakeSegment2.x;
+    let yEquals = snakeSegment1.y === snakeSegment2.y;
+    return xEquals && yEquals;
+}
 
 function main(snake){
-    setInterval( function() {
+    document.addEventListener('keydown', function(key){
+        let keyValue = key.key;
+        switch(keyValue){
+            case 'ArrowUp':
+                snake[0].dir = "up";
+                break;
+            case 'ArrowDown':
+                snake[0].dir = "down";
+                break;
+            case 'ArrowLeft':
+                snake[0].dir = "left";
+                break;
+            case 'ArrowRight':
+                snake[0].dir = "right";
+                break;
+        }});
+    intervalID = setInterval( function() {
         renderSnake(snake);
-        snake = addKeyHandler(snake);
         snake = moveSnake(snake);
-        
-    }, 1000)
+        if (gameOver(snake)){
+            clearInterval(intervalID);
+            displayGameOver();
+        }
+    }, 100)
 }
 
+let mainSnake = [ {x:40, y:40, dir:"right"}, {x:30, y:40, dir:"right"}, {x:20, y:40, dir:"right"} ]
 main(mainSnake);
 
 try {
 exports.moveSnakeSegment = moveSnakeSegment
 exports.moveSnake = moveSnake
+exports.overItself = overItself
+exports.snakeSegmentEquals = snakeSegmentEquals
 } catch {
     console.log("exports is not defined when run in the browser")
 }
--- app.test.js
+++ app.test.js
@@ -7,41 +7,59 @@ const app = require('./app')
 
 let snakeSegmentExample1 = {x:10, y:10, dir: "right"};
 let snakeSegmentExample2 = {x:20, y:10, dir: "right"};
-let snakeSegmentExample3 = {x:30, y:10, dir: "right"};
-let snakeSegmentExample3b = {x:40, y:10, dir:"right"};
-let snakeSegmentExample4 = {x:10, y:20, dir: "down"};
-let snakeSegmentExample5 = {x:10, y:30, dir:"down"};
-let snakeSegmentExample6 = {x:50, y:50, dir: "left"};
-let snakeSegmentExample7 = {x:40, y:50, dir: "left"};
-let snakeSegmentExample8 = {x:50, y:50, dir: "up"};
-let snakeSegmentExample9 = {x:50, y:40, dir: "up"};
-
-let snakeExample1 = [snakeSegmentExample3, snakeSegmentExample2, snakeSegmentExample1];
-let snakeExample2 = [snakeSegmentExample3b, snakeSegmentExample3, snakeSegmentExample2];
-
-let snakeExample3 = [ {x:40, y:40, dir:"right"}, {x:30, y:40, dir:"right"} ]
-let snakeExample4 = [ {x:50, y:40, dir:"right"}, {x:40, y:40, dir:"right"} ]
-
-test('moveSnake basic test', () => {
-    expect(app.moveSnake(snakeExample1)).toStrictEqual(snakeExample2);
-})
-
-test('moveSnake basic test 2', () => {
-    expect(app.moveSnake(snakeExample3)).toStrictEqual(snakeExample4);
-})
+let snakeSegmentExample3 = {x:10, y:20, dir: "down"};
+let snakeSegmentExample4 = {x:10, y:30, dir:"down"};
+let snakeSegmentExample5 = {x:50, y:50, dir: "left"};
+let snakeSegmentExample6 = {x:40, y:50, dir: "left"};
+let snakeSegmentExample7 = {x:50, y:50, dir: "up"};
+let snakeSegmentExample8 = {x:50, y:40, dir: "up"};
 
 test('moveSnakeSegment towards the right', () => {
     expect(app.moveSnakeSegment(snakeSegmentExample1)).toStrictEqual(snakeSegmentExample2);
 })
 
 test('moveSnakeSegment down', () => {
-    expect(app.moveSnakeSegment(snakeSegmentExample4)).toStrictEqual(snakeSegmentExample5);
+    expect(app.moveSnakeSegment(snakeSegmentExample3)).toStrictEqual(snakeSegmentExample4);
 })
 
 test('moveSnakeSegment towards the left', () => {
-    expect(app.moveSnakeSegment(snakeSegmentExample6)).toStrictEqual(snakeSegmentExample7);
+    expect(app.moveSnakeSegment(snakeSegmentExample5)).toStrictEqual(snakeSegmentExample6);
 })
 
 test('moveSnakeSegment up', () => {
-    expect(app.moveSnakeSegment(snakeSegmentExample8)).toStrictEqual(snakeSegmentExample9);
+    expect(app.moveSnakeSegment(snakeSegmentExample7)).toStrictEqual(snakeSegmentExample8);
+})
+
+let snakeEx1 = [ {x:10, y:20, dir:"right"}, {x:20, y: 20, dir:"left"}, {x:30, y:20, dir:"left"}];
+let snakeEx2 = [ {x:20, y:20, dir:"right"}, {x:10, y:20, dir:"right"}, {x:20, y:20, dir:"left"}];
+
+test('moveSnake basic test 1', () => {
+    expect(app.moveSnake(snakeEx1)).toStrictEqual(snakeEx2);
+})
+
+let snakeSegEx1 = {x:10, y:10, dir: "right"};
+let snakeSegEx2 = {x:20, y:10, dir: "right"};
+let snakeSegEx3 = {x:10, y:20, dir: "left"};
+let snakeSegEx4 = {x:20, y:10, dir: "left"}
+
+test('snakeSegmentEquals basic test 1', () => {
+    expect(app.snakeSegmentEquals(snakeSegEx1, snakeSegEx2)).toBe(false);
+})
+
+test('snakeSegmentEquals basic test 2', () => {
+    expect(app.snakeSegmentEquals(snakeSegEx1, snakeSegEx1)).toBe(true);
+})
+
+test('snakeSegmentEquals basic test 3', () => {
+    expect(app.snakeSegmentEquals(snakeSegEx1, snakeSegEx3)).toBe(false);
+})
+
+test('snakeSegmentEquals basic test 4', () => {
+    expect(app.snakeSegmentEquals(snakeSegEx2, snakeSegEx4)).toBe(true);
+})
+
+let snakeEx3 = [ {x:20, y:20, dir:"right"}, {x:10, y:20, dir:"right"}, {x:20, y:20, dir:"left"}];
+
+test('overItself basic test 1', () => {
+    expect(app.overItself(snakeEx3)).toBe(true);
 })
